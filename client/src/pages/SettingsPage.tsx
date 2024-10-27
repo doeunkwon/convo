c3c8752d5dfe6378073adc853b2a1c7eb6deb8d2
Doeun Kwon <dekwooon@gmail.com>
2024-10-27 14:51:08 -0700

    implement pop up for level selection

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -3,6 +3,7 @@ import { signOut, deleteUser } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import { useState } from "react";
 
 interface SettingsPageProps {
   handleLevelChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
@@ -15,6 +16,12 @@ function SettingsPage({
   handleLevelSet,
   level,
 }: SettingsPageProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -51,27 +58,69 @@ function SettingsPage({
 
   return (
     <main className="settings-page">
-      <p>Social Skill Level</p>
-      <select
-        onChange={handleLevelChange}
-        className="settings-level-picker"
-        value={level}
-      >
-        <option value="">Select a level</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <Button gradient={false} text="Set level" onClick={handleLevelSet} />
-      <div className="settings-divider" />
-      <Button gradient={false} text="Log out" onClick={handleLogout} />
+      <Button gradient={false} text="Set Level" onClick={togglePopup} />
+      <Button gradient={false} text="Log Out" onClick={handleLogout} />
       <Button
         gradient={false}
-        text="Delete account"
+        text="Delete Account"
         onClick={handleDeleteAccount}
       />
+      {isOpen && (
+        <div className="settings-overlay" onClick={togglePopup}>
+          <div className="settings-popup" onClick={(e) => e.stopPropagation()}>
+            <section className="settings-popup-header">
+              <h3 style={{ color: "var(--text-color)" }}>
+                How comfortable are you in social situations?
+              </h3>
+              <p>Choose the level that feels most like you:</p>
+            </section>
+            <section className="settings-descriptions">
+              <div>
+                <b>1. Beginner</b>
+                <br />
+                Socializing feels a bit out of my comfort zone. I prefer to
+                observe rather than start conversations.
+              </div>
+              <div>
+                <b>2. Casual Connector</b>
+                <br />I enjoy short, casual chats but sometimes feel unsure
+                about keeping conversations going.
+              </div>
+              <div>
+                <b>3. Social Starter</b>
+                <br />
+                I’m comfortable in most social settings, happy to chat but still
+                warming up to leading conversations.
+              </div>
+              <div>
+                <b>4. Skilled Conversationalist</b>
+                <br />I feel relaxed and natural in social situations, and I
+                often take the initiative to keep conversations engaging.
+              </div>
+              <div>
+                <b>5. Social Pro</b>
+                <br />
+                I’m energized by social interactions, can navigate conversations
+                with ease, and connect effortlessly with new people.
+              </div>
+            </section>
+            <section className="settings-popup-picker">
+              <select
+                onChange={handleLevelChange}
+                className="settings-level-picker"
+                value={level}
+              >
+                <option value="1">1. Beginner</option>
+                <option value="2">2. Casual Connector</option>
+                <option value="3">3. Social Starter</option>
+                <option value="4">4. Skilled Conversationalist</option>
+                <option value="5">5. Social Pro</option>
+              </select>
+              <button className="orange-button">Set level</button>
+            </section>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
